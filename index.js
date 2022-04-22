29b8bacabeac5bfc15374e3abfe565391bd81647
Ribhu Ghosh <ribhughosh7@gmail.com>
2022-04-22 20:00:36 +0530

    day2

---
+++ index.js
@@ -0,0 +1,20 @@
+var age = 25;
+// console.log(age);
+
+var age;
+age = 26;
+// console.log(age);
+age = 27;
+console.log(age);
+
+var age = 29;
+console.log(age);
+const lname = "Gupta";
+// lname = "Vinci";
+console.log(lname);
+let fName = "Sidhant";//declared and initialized in the same line
+let fname;
+fname = "Lakhi";
+console.log(fname);
+fname = "Lakhwinder";
+console.log(fname);
